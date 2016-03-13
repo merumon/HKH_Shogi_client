cd5339d0f2733056d3f3da4cca4758974e4d5c6f
merumon <obj.mvalue@gmail.com>
2016-03-13 23:33:11 +0900

    意味のないGameController（シーン上で非アクティブ）

---
+++ HKH_Shogi/Assets/Scripts/GameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    [SerializeField] CanvasScaler rootCanvasScaler; // TODO 別階層のオブジェクト参照をやめる
+
+    void Awake()
+    {
+        Initialize ();
+    }
+        
+    void Initialize()
+    {
+#if UNITY_WEBGL
+        rootCanvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ConstantPixelSize;
+#elif UNITY_EDITOR
+        rootCanvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+        #else 
+        rootCanvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+#endif
+    }
+}
