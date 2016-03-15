4518efe8c9288c169e9b2086fcf3ad8cc7b530e2
merumon <obj.mvalue@gmail.com>
2016-03-16 02:07:10 +0900

    ルームに入ってマッチングしたらラベル表示

--- HKH_Shogi/Assets/Scripts/MenuButtonsPanel.cs
+++ HKH_Shogi/Assets/Scripts/MenuButtonsPanel.cs
@@ -1,10 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class MenuButtonsPanel : MonoBehaviour
 {
-    public void OnTapTestButton()
+    [SerializeField] Button button1;
+    [SerializeField] Button button2;
+    [SerializeField] Text matchedText;
+
+    public void OnTapTestButton1()
     {
-        Debug.Log ("test.");
+        button1.enabled = false;
+        GameController.Instance.MatchingClient.StartMatching (1, onMatched:() => {
+            matchedText.text = "Room1 マッチング成功！";
+        }, onFailed: () => {
+            matchedText.text = "接続失敗...";
+        });
+    }
+
+    public void OnTapTestButton2()
+    {
+        button2.enabled = false;
+        GameController.Instance.MatchingClient.StartMatching (2, onMatched: () => {
+            matchedText.text = "Room2 マッチング成功！";
+        }, onFailed: () => {
+            matchedText.text = "接続失敗...";
+        });
     }
 }
